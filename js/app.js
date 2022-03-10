4f0c0e61d4f4ae4841a69327b6ba6544d0f2f5ae
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-11 00:12:49 +0600

    report post fixed

--- js/app.js
+++ js/app.js
@@ -152,7 +152,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById("reported").appendChild(div);
     });
